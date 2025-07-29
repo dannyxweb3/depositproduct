8909c62c5834d6eb8473791188a17d3d0a81d185
dannyxweb3 <dannyxuweb3@gmail.com>
2025-07-29 20:24:08 +0800

    Update app-for-mycontract.js

--- app-for-mycontract.js
+++ app-for-mycontract.js
@@ -1,1 +1,14 @@
 
+const contract = new ethers.Contract(contractAddress, abi, signer);
+
+// 1. 授权合约可以转账USDT
+await usdt.approve(contract.address, depositAmount);
+
+// 2. 存入理财产品
+await contract.deposit(depositAmount);
+
+// 3. 过一天后，领取利息
+await contract.claimInterest();
+
+// 4. 30天后提取本金 + 剩余利息
+await contract.withdraw();
